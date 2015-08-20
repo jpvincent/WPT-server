e4e7b3d30c9638f9c58abd1ea1f0462c4551d019
jpvincent <jpvirtual@yahoo.com>
2015-08-20 11:20:32 +0200

    If allowPrivate has been checked, we dont even check the validity of the domain name

--- runtest.php
+++ runtest.php
@@ -1257,9 +1257,9 @@ function ValidateURL(&$url, &$error, &$settings)
     $parts = parse_url($url);
     $host = $parts['host'];
 
-    if( strpos($url, ' ') !== FALSE || strpos($url, '>') !== FALSE || strpos($url, '<') !== FALSE )
+    if( (strpos($url, ' ') !== FALSE || strpos($url, '>') !== FALSE || strpos($url, '<') !== FALSE) && !$settings['allowPrivate'] )
         $error = "Please enter a Valid URL.  <b>" . htmlspecialchars($url) . "</b> is not a valid URL";
-    elseif( strpos($host, '.') === FALSE )
+    elseif( strpos($host, '.') === FALSE && !$settings['allowPrivate'] )
         $error = "Please enter a Valid URL.  <b>" . htmlspecialchars($host) . "</b> is not a valid Internet host name";
     elseif( (!strcmp($host, "127.0.0.1") || !strncmp($host, "192.168.", 8)  || !strncmp($host, "10.", 3)) && !$settings['allowPrivate'] )
         $error = "You can not test <b>$host</b> from the public Internet.  Your web site needs to be hosted on the public Internet for testing";
